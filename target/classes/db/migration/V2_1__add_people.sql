aa4dd8c391231c65e1e8d3377e2559bcb98964b4
ravikar <kravindra@neevtech.com>
2015-12-01 18:25:27 +0530

    first commit

---
+++ target/classes/db/migration/V2_1__add_people.sql
@@ -0,0 +1,3 @@
+insert into PERSON (ID, NAME) values (7, 'name4');
+insert into PERSON (ID, NAME) values (8, 'name5o');
+insert into PERSON (ID, NAME) values (9, 'name6');
---
+++ target/classes/db/migration/V2__add_people.sql
@@ -0,0 +1,4 @@
+insert into PERSON (ID, NAME) values (4, 'name4');
+insert into PERSON (ID, NAME) values (5, 'name5o');
+insert into PERSON (ID, NAME) values (6, 'name6');
+
